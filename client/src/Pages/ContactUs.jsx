28975ca9c05271059b3f3b639af61fe18418a8a4
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-08-06 21:45:20 +0530

    Made Contact us page responsive

--- client/src/Pages/ContactUs.jsx
+++ client/src/Pages/ContactUs.jsx
@@ -35,13 +35,13 @@ const ContactUs = () => {
     }
     
     return (
-        <div className="py-30 bg-base-300 flex flex-col items-center">
-            <form className="flex flex-col items-center text-sm bg-base-100 w-250 p-6 rounded-lg shadow-lg" onSubmit={handleMail}>
+        <div className="py-30 bg-base-300 flex flex-col items-center px-2">
+            <form className="flex flex-col items-center text-sm bg-base-100 w-250 max-md:w-80 max-md:px-2  max-lg:w-170 overflow-hidden p-6 rounded-lg shadow-lg" onSubmit={handleMail}>
             <p className="text-lg text-primary font-medium pb-2">Contact Us</p>
-            <h1 className="text-4xl font-semibold text-base-content/90 pb-4">Get in touch with us</h1>
-            <p className="text-sm text-base-content/60 text-center pb-10 w-150">We’re always here to help! Whether you have questions about your order, need help choosing the right product, or just want to say hello — don’t hesitate to reach out.</p>
+            <h1 className="text-4xl font-semibold text-base-content/90 pb-4 text-center">Get in touch with us</h1>
+            <p className="text-sm text-base-content/60 text-center pb-10 text-justify px-3">We’re always here to help! Whether you have questions about your order, need help choosing the right product, or just want to say hello — don’t hesitate to reach out.</p>
             
-            <div className="flex flex-col md:flex-row items-center gap-8 w-[350px] md:w-[700px]">
+            <div className="flex flex-col lg:flex-row items-center gap-8 w-full px-2">
                 <div className="w-full">
                     <label className="text-base-content/70" htmlFor="name">Your Name</label>
                     <input className="h-12 p-2 mt-2 w-full border border-base-content/80 rounded outline-none focus:border-primary/80" type="text" name="name" onChange={handleChange} required />
@@ -52,7 +52,7 @@ const ContactUs = () => {
                 </div>
             </div>
         
-            <div className="mt-6 w-[350px] md:w-[700px]">
+            <div className="mt-6 w-full px-2">
                 <label className="text-base-content/70" htmlFor="name">Message</label>
                 <textarea className="w-full mt-2 p-2 h-40 border border-base-content/80 rounded resize-none outline-none focus:border-primary/80" name="message" onChange={handleChange} required></textarea>
             </div>
