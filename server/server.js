8330f24b50ed4ec06cba176b23008d8bfdb7c104
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-08-10 16:21:04 +0530

    modified front end url for solving cors issue

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const path = require('path');
 app.use(express.json());
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:"quenzy.netlify.app",
         credentials: true,
     }
 ));
